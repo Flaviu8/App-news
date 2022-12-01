cde8fb3624cc7d713892232e5a93a4abd8cf0594
khatuna120 <shaveshovak@gmail.com>
2022-12-01 20:37:03 +0100

    added btn

--- src/components/social_icons/SocialIcons.js
+++ src/components/social_icons/SocialIcons.js
@@ -1,9 +1,15 @@
+import { useState } from 'react';
 import { Link } from "@mui/material";
 import FacebookIcon from "@mui/icons-material/Facebook";
 import InstagramIcon from "@mui/icons-material/Instagram";
 import TwitterIcon from "@mui/icons-material/Twitter";
+import SearchComponent from '../searchButton/SearchComponent';
 
 const SocialIcons = () => {
+  const [show, setShow] = useState(false)
+  const handleOnClick = () => {
+    setShow(!show);
+  }
   return (
     <div>
       <Link>
@@ -15,6 +21,8 @@ const SocialIcons = () => {
       <Link>
         <TwitterIcon />
       </Link>
+      <button onClick={handleOnClick}>Click</button>
+      {show ? <SearchComponent /> : ''}
     </div>
   );
 };
